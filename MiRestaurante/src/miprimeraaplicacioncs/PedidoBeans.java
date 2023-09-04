3180673b5f2abca03478a7acd41776c84532b469
s24alcivar <143277861+s24alcivar@users.noreply.github.com>
2023-09-04 18:08:21 -0500

    Add files via upload

---
+++ MiRestaurante/src/miprimeraaplicacioncs/PedidoBeans.java
@@ -0,0 +1,122 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package miprimeraaplicacioncs;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Usuario
+ */
+public class PedidoBeans {
+    private int id_pedido;
+    private int id_plato;
+    private int cantidad;
+    private double precio;
+    private double total;
+    private accesobd bd;
+
+    public PedidoBeans() throws Exception {
+        bd = new accesobd("localhost", "root", "", "restaurantesj");//1  ta
+        bd.conectarBD();
+        //T= ta
+    }
+
+    public int getId_pedido() {
+        return id_pedido;
+    }
+
+    public void setId_pedido(int id_pedido) {
+        this.id_pedido = id_pedido;//1  ta
+        //T= ta
+    }
+
+    public int getId_plato() {
+        return id_plato;
+    }
+
+    public void setId_plato(int id_plato) {
+        this.id_plato = id_plato;//1  ta
+        //T= ta
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;//1  ta
+        //T= ta
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;//1  ta
+        //T= ta
+    }
+    
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;//1  ta
+        //T= ta
+    }
+    
+     public int Incremento_Pedido() throws SQLException {
+        int inc = 0;//1  ta
+        ResultSet rs = bd.consultaBD("SELECT max(idpedido) as num FROM pedido;");//2  ta
+        if (rs.next()) {//3  n(tc)
+            inc = rs.getInt(1) + 1;//4  n(ta+ to)
+        } else {
+            inc = 1;//5  ta
+        }
+        return inc;
+        //Tiempo
+        //T= ta + ta + ntc + nta + nto
+        //T= 2ta + n(ta + tc + to)
+    }
+
+    
+    public void Insertar_Pedido() throws SQLException {
+        String cadena = "insert into pedido values('" + Incremento_Pedido()//1  ta
+                + "','" + getId_plato()+ "','" + getCantidad()+ "','"  + getPrecio()+ "','" + getTotal()+ "')";
+        bd.ActualizarBD(cadena);
+        //T= ta
+    }
+
+    public void Actualizar_Pedido() throws SQLException {
+        // Implementar la lógica para actualizar el pedido
+    }
+
+    public void Eliminar_Pedido() throws SQLException{
+        // Implementar la lógica para eliminar el pedido
+    }
+
+    public ResultSet consultaTabla(String sql) throws SQLException {
+        return bd.consultaBD(sql);
+    }
+
+    public void Consultar_Pedido() throws SQLException {
+        ResultSet rs = bd.consultaBD("select * from pedido");//1  ta
+        while (rs.next()) {//2  n(tc)
+            System.out.print(rs.getInt(1) + " ");
+            System.out.print(rs.getString(2) + " ");
+            System.out.print(rs.getString(3) + " ");
+            System.out.print(rs.getString(4) + " ");
+            System.out.print(rs.getString(5) + " ");
+            System.out.print(rs.getString(6) + " ");
+            System.out.println("");
+        }
+        //Tiempo = ta + ntc
+    }
+}
+
---
+++ MiRestaurante/src/miprimeraaplicacioncs/Platos.java
@@ -0,0 +1,141 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package miprimeraaplicacioncs;
+
+/**
+ *
+ * @author USUARIO
+ */
+
+import java.sql.*;
+
+public class Platos {
+    private int Id_Platos;
+    private int Id_Pedido;
+    private String nombres;
+    private String descripcion;
+    private int precio;
+    accesobd bd;
+
+    public Platos() throws Exception {
+        bd = new accesobd("localhost", "root", "", "restaurantesj");//  ta
+        bd.conectarBD();
+        //T= ta
+    }
+
+    public int getId_Platos() {
+        return Id_Platos;
+    }
+
+    public void setId_Platos(int Id_Platos) {
+        this.Id_Platos = Id_Platos;//1  ta
+        //T= ta
+    }
+
+    public int getId_Pedido() {
+        return Id_Pedido;
+    }
+
+    public void setId_Pedido(int Id_Pedido) {
+        this.Id_Pedido = Id_Pedido;//1  ta
+        //T= ta
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;//1  ta
+        //T= ta
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;//1  ta
+        //T= ta
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;//1  ta
+        //T= ta
+    }
+
+    public accesobd getBd() {
+        return bd;
+    }
+
+    public void setBd(accesobd bd) {
+        this.bd = bd;//1  ta
+        //T= ta
+    }
+
+  
+    public int Incremento_Platos() throws SQLException {
+        int inc;
+        ResultSet rs;
+        rs = bd.consultaBD("SELECT max(idplatos) as num FROM platos;");//  ta
+        if (rs.next()) {//2  n(tc)
+            inc = rs.getInt(1) + 1;//3  n(ta + to)
+        } else {
+            inc = 1;//4  ta
+        }
+        return inc;
+        //Tiempo
+        //T= ta + ntc + nta + nto + ta
+        //T= 2ta + nta + ntc + nto
+        //T= 2ta + n(ta + tc +to)
+    }
+
+    
+    public void Insertar_Platos() throws SQLException {
+        String cadena = "insert into platos values('" + Incremento_Platos() //1  ta + to
+                + "','" + getId_Pedido() + "','" + getNombres() + "','"  + getDescripcion() + "','" + getPrecio() + "')";
+        bd.ActualizarBD(cadena);
+        //Tiempo=  ta + to
+
+    }
+
+    public void Actualizar_Platos() throws SQLException {
+        String cadena = "update platos set idpedido='"+ getId_Pedido()//1  ta + to
+                +"', nombre= '" + getNombres() +"',descripcion= '" + getDescripcion() +"',precio= '" +getPrecio()+ "'where idplatos='" + getId_Platos()+ "' ";
+        bd.ActualizarBD(cadena);
+        //Tiempo=  ta + to
+    }
+
+    public void Eliminar_Platos() throws SQLException{
+        String cadena = "delete from platos where idplatos ='" + getId_Platos()+ "'";//1  ta + to
+        bd.ActualizarBD(cadena);
+        //Tiempo = ta + to
+    }
+
+    public ResultSet consultaTabla(String sql) throws SQLException {
+        return bd.consultaBD(sql);
+    }
+
+    public void Consultar_Platos() throws SQLException {
+        ResultSet rs;
+        rs = bd.consultaBD("select * from platos");//1  ta
+        while (rs.next()) {//2  n(tc)
+            System.out.print(rs.getInt(1) + " ");
+            System.out.print(rs.getString(2) + " ");
+            System.out.print(rs.getString(3) + " ");
+            System.out.print(rs.getString(4) + " ");
+            System.out.print(rs.getString(5) + " ");
+            System.out.print(rs.getString(6) + " ");
+            System.out.println("");
+        }
+        //Tiempo=  ta + ntc
+    }
+
+}
